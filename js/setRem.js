2f963d35f99d4bd1da39b27801e812072b20d1f7
xiaokai <1020849724@qq.com>
2022-11-15 22:35:21 +0800

    最大最小宽设置完成 小凯

--- js/setRem.js
+++ js/setRem.js
@@ -3,6 +3,8 @@ let html = document.documentElement;
 function setRem() {
   let ui_w = 375;
   let cl_w = document.documentElement.clientWidth || document.body.clientWidth;
+  cl_w = cl_w < 325 ? 325 : cl_w;
+  cl_w = cl_w > 800 ? 800 : cl_w;
   html.style.fontSize = (cl_w / ui_w) * 10 + "px";
 }
 window.addEventListener("load", setRem);
